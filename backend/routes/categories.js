d483bd926b0b7dddb5ca744eff22371befd9e4a4
yaminikota <yamini.kota466@gmail.com>
2023-11-29 13:51:10 +0530

    put & get methods in categories

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -3,15 +3,45 @@ const express = require('express');
 const router = express.Router();
 
 // http://localhost:3000/api/v1/categories
+//Get 
 router.get('/', async (req, res) => {
     const categoriesList = await Category.find();
 
     if (!categoriesList) {
         res.status(500).json({ success: false });
     }
-    res.send(categoriesList);
+    res.status(200).send(categoriesList);
 });
 
+// Put
+router.put('/:id', async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        },
+        { new: true }
+    )
+
+    if (!category)
+        return req.status(400).send('The Category cannot be updated!');
+
+    res.status(200).send(category);
+});
+
+//GetById
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+        res.status(500).json({ message: 'The Category with the given ID was not found!' });
+    }
+    return res.status(200).send(category);
+});
+
+//Post
 router.post('/', async (req, res) => {
     let category = new Category({
         name: req.body.name,
@@ -26,6 +56,7 @@ router.post('/', async (req, res) => {
     return res.send(category);
 });
 
+//Delete
 router.delete('/:id', (req, res) => {
     Category.findOneAndDelete({ _id: req.params.id }).then((category) => {
         if (category) {
