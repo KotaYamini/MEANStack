16bedf917399a0995e9f26b4074f22dabc19cbfb
yaminikota <yamini.kota466@gmail.com>
2023-12-12 15:22:29 +0530

    password comparision

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -72,6 +72,21 @@ router.put('/:id', async (req, res) => {
         return req.status(400).send('The User cannot be updated!');
 
     res.status(200).send(user);
-})
+});
+
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return res.status(400).send('The user not found');
+    }
+    // compare the stored password in the database with the user entered password
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        res.status(200).send('User Authenticated');
+    } else {
+        res.status(400).send('Password is wrong!');
+    }
+
+});
 
 module.exports = router;
